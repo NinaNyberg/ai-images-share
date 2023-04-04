6523e9b32e0d2aeee9c4476e71426512e83cbf9a
Nina N <nina.b.nyberg@gmail.com>
2023-04-04 23:38:40 +0300

    Post routes setup

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,15 +6,23 @@ dotenv.config();
 
 const router = express.Router();
 
+// openai setup
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
 
+// routes
+
+// get route to check if backend loads
+
 router.get('/', (req, res) => {
   res.send('Hello from DALL-E');
 });
+
+// post route for sending prompt to ai to generate an image using openai createImage method
 
 router.post('/', (req, res) => {
   const prompt = req.body.prompt;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,4 +7,16 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// get all posts
+router.get('/', (req, res) => {});
+
+// create a post
+router.post('/', (req, res) => {});
+
 export default router;
