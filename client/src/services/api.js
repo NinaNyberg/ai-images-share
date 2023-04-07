6e0b56cb954ff59779921ecbee4a0062a5bffe1b
Nina N <nina.b.nyberg@gmail.com>
2023-04-07 22:34:34 +0300

    Change in api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_REST_API_URL
-  //   baseURL: 'http://localhost:8080/api/v1'
   //   withCredentials: true
 });
 
