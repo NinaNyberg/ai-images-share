1b39ad4c34f041f6f97823155f08323a8466c15d
Nina N <nina.b.nyberg@gmail.com>
2023-04-07 14:19:10 +0300

    BaseURL change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //   baseURL: process.env.REACT_APP_REST_API_URL,
-  baseURL: 'http://localhost:8080/api/v1'
+  baseURL: import.meta.env.VITE_REST_API_URL
   //   withCredentials: true
 });
 
